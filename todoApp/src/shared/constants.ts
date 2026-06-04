3e4581a7997e7252beba177cb9dedf4a3e958c70
Julian Dario Luna Patiño <judlup@gmail.com>
2026-06-04 16:51:40 -0500

    .

--- todoApp/src/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_STATUS = 'todo' as const;
-export const DEFAULT_PRIORITY = 'medium' as const;
-
-export const STATUSES = ['todo', 'in-progress', 'done'] as const;
-export const PRIORITIES = ['low', 'medium', 'high'] as const;
--- todoApp/src/shared/types.ts
+++
@@ -1,2 +0,0 @@
-export type Status = 'todo' | 'in-progress' | 'done';
-export type Priority = 'low' | 'medium' | 'high';
